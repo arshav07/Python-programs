267f8dcc62318a26804bdfb72d07c0f636a0d6b3
arshav07 <arshavinitha123@gmail.com>
2026-08-03 20:28:20 +0530

    Create loops_practice.py

---
+++ loops_practice.py
@@ -0,0 +1,17 @@
+# For Loop Example
+print("Numbers from 1 to 5")
+for i in range(1, 6):
+    print(i)
+
+# While Loop Example
+print("Numbers from 5 to 1")
+i = 5
+while i >= 1:
+    print(i)
+    i = i - 1
+
+# Even Numbers
+print("Even numbers from 1 to 10")
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
